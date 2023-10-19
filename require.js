98cdd7b9b61c99d82889a772dd0d085cd5201e2f
Daniel Matthies <mageluingil@gmail.com>
2023-10-19 10:15:48 -0600

    Reorganize functions

--- require.js
+++ require.js
@@ -24,6 +24,39 @@ var require, module, exports;
 		this.exports = {};
 	};
 	
+	/****************
+	 * Misc helpers *
+	 ****************/
+	
+	/**
+	 * Initialize JS search paths. Uses array to conform with CommonJS spec.
+	 */
+	let initPaths = function() {
+		const PATH = global('JS_PATH') || dirname(global('CommonJS'));
+		
+		// Use a set to remove possible duplicates
+		let paths = new Set();
+		for (let path of PATH.split(':').map(normalizePath)) {
+			// Remove invalid paths now to save time later
+			if (!stat(path)) continue;
+			paths.add(path);
+		}
+		return Array.from(paths);
+	};
+	
+	let cloneEnumerableProperties = function(target, source) {
+		return Object.keys(source).reduce(
+			(_, name) => Object.defineProperty(target, name, Object.getOwnPropertyDescriptor(source, name)),
+			undefined
+		);
+	};
+	
+	let safeParseJSON = function(str) {
+		try {
+			return JSON.parse(str);
+		} catch (e) {}
+	};
+	
 	/*********************
 	 * File path helpers *
 	 *********************/
@@ -99,17 +132,39 @@ var require, module, exports;
 	 ***********************/
 	
 	/**
-	 * Resolve a filepath without a file extension to an existing file
+	 * Run system call to find the type of a file
 	 *
-	 * @param {String} filepath The path to the file
-	 * @param {Boolean} check_wo_ext Whether to check for the file without
-	 *  appending an extension
-	 * @return {String|undefined}
+	 * @param {String} filepath  The file path to check
+	 * @return {String}
 	 */
-	let resolveExtension = function(filepath, check_wo_ext) {
-		if (check_wo_ext && stat(filepath) == 'regular file') return filepath;
-		for (let ext of ['.js', '.json']) {
-			if (stat(filepath + ext) == 'regular file') return filepath + ext;
+	let stat = function(filepath) {
+		// Quote path for safe shell usage
+		filepath = "'" + filepath.replace(/'/g, "'\\''") + "'";
+		// `stat` isn't always available in android, so use a POSIX safe test
+		return shell(`
+			if [ -d ${filepath} ]; then
+				echo "directory"
+			elif [ -f ${filepath} ]; then
+				echo "regular file"
+			fi
+		`);
+	};
+	
+	/**
+	 * Determine which directories to search based on the module ID and current directory
+	 */
+	let resolveSearchPaths = function(module_id, directory) {
+		if (module_id[0] == '/') {
+			// Absolute identifier; don't search within any paths
+			return [''];
+		} else if (/^\.{1,2}\//.test(module_id) && directory && directory != '.') {
+			// Relative identifier; only search within current directory
+			return [directory];
+		} else if (directory && directory != '.') {
+			// Top-level identifier; also search within current directory
+			return require.paths.concat([directory]);
+		} else {
+			return require.paths;
 		}
 	};
 	
@@ -127,18 +182,7 @@ var require, module, exports;
 		if (cache[key]) return cache[key];
 		
 		// For most module identifiers, search within predefined paths
-		let paths = require.paths;
-		if (module_id[0] == '/') {
-			// Absolute identifier; don't search within any paths
-			paths = [ '' ];
-		} else if (/^\.{1,2}\//.test(module_id) && directory && directory != '.') {
-			// Relative identifier; only search within current directory
-			paths = [ directory ];
-		} else if (directory && directory != '.') {
-			// Top-level identifier; also search within current directory
-			paths.push(directory);
-		}
-		
+		let paths = resolveSearchPaths(module_id, directory);
 		for (let path of paths) {
 			let filepath = normalizePath(joinPath(path, module_id));
 			let filetype = stat(filepath);
@@ -190,60 +234,23 @@ var require, module, exports;
 	};
 	
 	/**
-	 * Run system call to find the type of a file
+	 * Resolve a filepath without a file extension to an existing file
 	 *
-	 * @param {String} filepath  The file path to check
-	 * @return {String}
+	 * @param {String} filepath The path to the file
+	 * @param {Boolean} check_wo_ext Whether to check for the file without
+	 *  appending an extension
+	 * @return {String|undefined}
 	 */
-	let stat = function(filepath) {
-		// Quote path for safe shell usage
-		filepath = "'" + filepath.replace(/'/g, "'\\''") + "'";
-		// `stat` isn't always available in android, so use a POSIX safe test
-		return shell(`
-			if [ -d ${filepath} ]; then
-				echo "directory"
-			elif [ -f ${filepath} ]; then
-				echo "regular file"
-			fi
-		`);
-	};
-	
-	/****************
-	 * Misc helpers *
-	 ****************/
-	
-	let cloneEnumerableProperties = function(target, source) {
-		return Object.keys(source).reduce(
-			(_, name) => Object.defineProperty(target, name, Object.getOwnPropertyDescriptor(source, name)),
-			undefined
-		);
-	};
-	
-	let safeParseJSON = function(str) {
-		try {
-			return JSON.parse(str);
-		} catch (e) {}
+	let resolveExtension = function(filepath, check_wo_ext) {
+		if (check_wo_ext && stat(filepath) == 'regular file') return filepath;
+		for (let ext of ['.js', '.json']) {
+			if (stat(filepath + ext) == 'regular file') return filepath + ext;
+		}
 	};
 	
 	/******************
 	 * Main functions *
 	 ******************/
-	
-	/**
-	 * Initialize search paths
-	 */
-	let initPaths = function() {
-		const PATH = global('JS_PATH') || dirname(global('CommonJS'));
-		
-		// Use a set to remove possible duplicates
-		let paths = new Set();
-		for (let path of PATH.split(':').map(normalizePath)) {
-			// Remove invalid paths now to save time later
-			if (!stat(path)) continue;
-			paths.add(path);
-		}
-		return Array.from(paths);
-	};
 	
 	/**
 	 * Parse a file as a module
